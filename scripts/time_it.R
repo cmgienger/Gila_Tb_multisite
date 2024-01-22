af5c0d822eacc53aecc6bb4d69e00e8024e35841
C.M. Gienger <cmgienger@gmail.com>
2024-01-22 14:28:31 -0600

    first commit

---
+++ scripts/time_it.R
@@ -0,0 +1,14 @@
+knitr::knit_hooks$set(time_it = local({
+  now <- NULL
+  function(before, options) {
+    if (before) {
+      # record the current time before each chunk
+      now <<- Sys.time()
+    } else {
+      # calculate the time difference after a chunk
+      res <- difftime(Sys.time(), now)
+      # return a character string to show the time
+      paste("Time for this code chunk to run:", res)
+    }
+  }
+}))
